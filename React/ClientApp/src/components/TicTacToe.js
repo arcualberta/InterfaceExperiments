564ff6b119f1679d6c7f9adca8ced3a470c3fe0a
Kamal Ranaweera <ranaweer@ualberta.ca>
2019-12-21 12:41:31 -0700

    Implemented winning criteria.

--- React/ClientApp/src/components/TicTacToe.js
+++ React/ClientApp/src/components/TicTacToe.js
@@ -24,7 +24,8 @@ export class TicTacToeBoard extends Component {
         this.state = {
             squares: Array(9).fill(null),
             xIsNext: true,
-            error: null
+            error: null,
+            winner: null
         };
         this.size = 5;
     }
@@ -37,25 +38,36 @@ export class TicTacToeBoard extends Component {
     }
 
     handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (squares[i]) {
-            this.setState({
-                error: 'Block is already taken. ' + (this.state.xIsNext ? 'X' : 'O') + ' lost a turn.',
-                xIsNext: !this.state.xIsNext
-            })
-        }
-        else {
-            squares[i] = this.state.xIsNext ? 'X' : 'O';
-            this.setState({
-                squares: squares,
-                error: null,
-                xIsNext: !this.state.xIsNext
-            });
+        if (this.state.winner == null) {
+            const squares = this.state.squares.slice();
+            if (squares[i]) {
+                this.setState({
+                    error: 'Block is already taken. ' + (this.state.xIsNext ? 'X' : 'O') + ' lost a turn.',
+                    xIsNext: !this.state.xIsNext
+                })
+            }
+            else {
+                squares[i] = this.state.xIsNext ? 'X' : 'O';
+                const winner = this.calcWinner(squares, this.size);
+                this.setState({
+                    squares: squares,
+                    error: null,
+                    xIsNext: !this.state.xIsNext,
+                    winner: winner
+                });
+            }
         }
     }
 
     render() {
-        const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        let status = null;
+        if (this.state.winner) {
+            status = 'Winner: ' + this.state.winner;
+        }
+        else {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         const error = this.state.error;
 
         let rows = [];
@@ -67,11 +79,65 @@ export class TicTacToeBoard extends Component {
             rows.push(<div className="board-row">{cols}</div>);
         }
 
+
         return (
             <div>
                 <div className="status">{status} <span className="error">{error}</span></div>
                 {rows}
             </div>
         );
+    }
+
+    calcWinner(squares, size) {
+
+        // Winning case 1: having a row filled with the same value
+        for (let r = 0; (r < size) ; ++r) {
+            let match = squares[r * size] != null;
+            for (let c = 0; (c < size - 1) && match; ++c) {
+                let idx = r * size + c;
+                match = (squares[idx]) == (squares[idx + 1]);
+            }
+
+            if (match)
+                return squares[r * this.size]
+        }
+
+        // Winning case 2: having a column filled with the same value
+        for (let c = 0; (c < size); ++c) {
+            let match = squares[c] != null;
+            for (let r = 0; (r < size - 1) && match; ++r) {
+                let idx = r * size + c;
+                match = (squares[idx] != null) && (squares[idx]) == (squares[idx + size]);
+            }
+
+            if (match)
+                return squares[c]
+        }
+
+        // Winning case 3: having the first diagonal filled with the same value
+        let match = squares[0] != null;
+        for (let i = 0; (i < size - 1) && match; ++i) {
+            let idx1 = i * size + i;
+            let idx2 = (i+1) * size + (i+1);
+
+            match = squares[idx1] == squares[idx2];
+        }
+        if (match)
+            return squares[0];
+
+        // Winning case 3: having the second diagonal filled with the same value
+        match = squares[size - 1] != null;
+        for (let i = 0; (i < size - 1) && match; ++i) {
+            let idx1 = i * size + size - 1 - i;
+            let idx2 = (i + 1) * size + (size - 2 - i);
+
+            match = squares[idx1] == squares[idx2];
+        }
+        if (match)
+            return squares[size - 1];
+
+
+        // None of the winning criteria is met
+        return null;
     }
 }
