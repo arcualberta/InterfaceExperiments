695b743306885cce3332e71f3c163ab694e726d0
Jill <jlovas@ualberta.ca>
2020-02-04 16:49:18 -0700

    cleaned up grid so it doesnt bounce when item is added

--- Vue/Vue/VueTest/src/components/Home.vue
+++ Vue/Vue/VueTest/src/components/Home.vue
@@ -2,9 +2,10 @@
     <div class="home">
         <h1>{{ msg }}</h1>
         <div>Next Player: {{players[turn]}}</div>
-        <div class="my-row" v-for="row of gridData.rows">
-            <div class="my-col" v-for="col of gridData.cols" v-on:click="placeToken"></div>
+        <div class="my-row" v-for="(row, rowIndex) in gridData.rows">
+            <div class="my-col" v-for="(col, colIndex) in gridData.cols" v-on:click="placeToken"></div>
         </div>
+        <button>Reset</button>
     </div>
 </template>
 
@@ -19,17 +20,25 @@
             rows: 3,
             cols: 3
         };
-        players: Array<String> = ['X', 'Y'];
+        players: Array<String> = ['X', 'O'];
         turn: number = 0;
 
               
         //places the player's token in the grid
-        placeToken() {
-            if (this.turn > 0) {
-                this.turn = 0;
-            } else {
-                this.turn = 1;
+        placeToken(element: any) {
+            if (element.target.innerHTML == '') {
+                element.target.innerHTML = this.players[this.turn];
+
+                if (this.turn > 0) {
+                    this.turn = 0;
+                } else {
+                    this.turn = 1;
+                }
+    
             }
+
+
+
         }
 
     }
@@ -43,7 +52,25 @@
     }
 
     .my-col{
-        padding: 20px;
+        width: 30px;
+        height: 30px;
         border: solid black 1px;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .square-filled{
+        border: 1px solid #999;
+        float: left;
+        font-size: 24px;
+        font-weight: bold;
+        line-height: 34px;
+        height: 34px;
+        margin-right: -1px;
+        margin-top: -1px;
+        padding: 0;
+        text-align: center;
+        width: 34px;
     }
 </style>
