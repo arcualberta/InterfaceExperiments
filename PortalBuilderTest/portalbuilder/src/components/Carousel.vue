ca89b37d55b54b058fd5e72d19192ea4d066f2b1
Jill <jlovas@ualberta.ca>
2020-02-18 15:53:17 -0700

    roughly added in dropzone for file upload in carousel. manually added in library's types, there will be an update at some point where they are included in the project

--- PortalBuilderTest/portalbuilder/src/components/Carousel.vue
+++ PortalBuilderTest/portalbuilder/src/components/Carousel.vue
@@ -2,19 +2,19 @@
 	extends ModalContentsBase.pug
 
 	block modalContentsHere
-		div
-		input(
-			type='checkbox'
-			name='agreement'
-			checked
-		)
+		vue-dropzone(ref="myVueDropzone" id="dropzone" :options="dropzoneOptions")
 </template>
 
 <script lang="ts">
 	import { Component, Prop, Vue } from 'vue-property-decorator';
 	import ModalContentsBase from './ModalContentsBase.vue';
+	import vue2Dropzone, { DropZoneOptions } from 'vue2-dropzone';
+	import 'vue2-dropzone/dist/vue2Dropzone.min.css';
 
 	@Component({
+		components: {
+			vueDropzone: vue2Dropzone
+		}
 	}) //['title', 'description', 'buttonText1', 'buttonText2', 'tabTitle1', 'tabTitle2', 'tabTitle3']
 	export default class Carousel extends ModalContentsBase implements ModalText{ 
 		title = "Image Carousel";
@@ -22,6 +22,13 @@
 		buttonText1 = "Close";
 		buttonText2 = "Button";
 		hasTabs = false;
+
+		dropzoneOptions: DropZoneOptions = {
+          url: 'https://httpbin.org/post',
+          thumbnailWidth: 150,
+          maxFilesize: 0.5,
+          headers: { "My-Awesome-Header": "header value" }
+      }
 
 		
 	}
